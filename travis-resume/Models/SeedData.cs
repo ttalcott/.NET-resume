be9abb5d3834cc8e77cb21f76a0fbfda94f36b22
Travis Talcott <ttalcott@lyradevelopment.com>
2017-02-18 00:44:11 -0700

    Started seed data class

---
+++ travis-resume/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using travis_resume.Data;
+
+namespace TravisResume.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context =
+               new ApplicationDbContext
+               (serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                //look for any records, if there are records do nothing
+                if (context.Jobs.Any())
+                {
+                    return;
+                }
+
+                context.Jobs.AddRange(
+                    new Jobs
+                    {
+
+                    }
+                    );
+            }
+        }
+    }
+}
