c41a1bfed81f47edcbf2183546b7a96b03579e3c
Travis Talcott <ttalcott@lyradevelopment.com>
2017-02-18 00:01:33 -0700

    started Job.cs class

---
+++ travis-resume/Models/Jobs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravisResume.Models
+{
+    public class Jobs
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Job Description")]
+        public string JobDescription { get; set; }
+
+        [Required]
+        [Display(Name ="Date Hired")]
+        public string DateHired { get; set; }
+
+
+    }
+}
