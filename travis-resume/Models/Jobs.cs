fad2f8311b7aff365be9a64929a41c21268e0afb
Travis Talcott <ttalcott@lyradevelopment.com>
2017-02-18 00:19:56 -0700

    Finished Jobs class for now

--- travis-resume/Models/Jobs.cs
+++ travis-resume/Models/Jobs.cs
@@ -11,13 +11,34 @@ namespace TravisResume.Models
         public int ID { get; set; }
 
         [Required]
-        [Display(Name = "Job Description")]
-        public string JobDescription { get; set; }
+        [Display(Name ="Company")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(30)]
+        public string companyName { get; set; }
 
         [Required]
-        [Display(Name ="Date Hired")]
-        public string DateHired { get; set; }
+        [Display(Name = "Title")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(30)]
+        public string jobTitle { get; set; }
 
+        [Required]
+        [Display(Name = "Job Description")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(30)]
+        public string jobDescription { get; set; }
+
+        [Required]
+        [Display(Name ="Start Date")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime dateHired { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime dateOver { get; set; }
 
     }
 }
