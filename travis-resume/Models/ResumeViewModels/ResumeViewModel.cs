035dcd3d6a95953b8636b94897337b45982b2f2a
Travis Talcott <ttalcott@lyradevelopment.com>
2017-04-01 13:25:06 -0600

    Adding styling and added view model

---
+++ travis-resume/Models/ResumeViewModels/ResumeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace travis_resume.Models.ResumeViewModels
+{
+    public class ResumeViewModel
+    {
+        public int Id { get; set; }
+
+
+    }
+}
